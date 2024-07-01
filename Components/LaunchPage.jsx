da3dc25b63037510fe38c0a1123d3e8de715658a
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-07-01 11:54:32 +0100

    Sets ups top layer components

---
+++ Components/LaunchPage.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function LaunchPage() {
+  return (
+    <View>
+      <Text>LaunchPage</Text>
+    </View>
+  )
+}
---
+++ Components/StatsPage.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function StatsPage() {
+  return (
+    <View>
+      <Text>StatsPage</Text>
+    </View>
+  )
+}
