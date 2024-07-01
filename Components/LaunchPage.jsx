0a1dc15dff4ec67adafa1517b59ffbcd09609e36
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-07-01 12:20:00 +0100

    Installed and tested React Navigation and installed dependencies required

--- Components/LaunchPage.jsx
+++ Components/LaunchPage.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-export default function LaunchPage() {
+
+export default function LaunchPage({navigation}) {
   return (
-    <View>
-      <Text>LaunchPage</Text>
-    </View>
+    <Button
+    title="Go to Jane's profile"
+    onPress={() =>
+      navigation.navigate('My Account', {name: 'Jane'})
+    }
+  />
   )
 }
+
+const AccountScreen = ({navigation, route}) => {
+    return <Text>This is {route.params.name}'s profile</Text>;
+  };
