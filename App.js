2fcb448aeb9e75ad34ebed4d0bb45f0f6b3e90ca
rociosmm <rociosmm@yahoo.com>
2024-07-03 23:33:04 +0100

    login working - default user removed

--- App.js
+++ App.js
@@ -1,33 +1,43 @@
 import { useContext, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+// components
 import Header from "./Components/Header";
 import LaunchPage from "./Components/LaunchPage";
 import MyAccount from "./Components/MyAccount";
 import Friends from "./Components/Friends";
 import NotificationsList from "./Components/NotificationsList";
 import QuizContainer from "./Components/QuizContainer";
+
 import AppNavigation from "./navigation/AppNavigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { UserContext } from "./context/UserContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
 const LoginNav = () => {
-  const Stack = createNativeStackNavigator();
+  /* const Stack = createNativeStackNavigator();
   return (
     <Stack.Navigator>
-      {/* Before login 
-     <Stack.Screen
-       name="Launch Page"
-       component={LaunchPage}
-       options={{ title: "Welcome" }}
-     />*/}
       <Stack.Screen name="QuizContainer" component={QuizContainer} />
       <Stack.Screen name="My Account" component={MyAccount} />
       <Stack.Screen name="Friends" component={Friends} />
       <Stack.Screen name="Notifications" component={NotificationsList} />
     </Stack.Navigator>
+  ); */
+
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Play" component={QuizContainer} />
+      <Tab.Screen name="My Account" component={MyAccount} />
+      <Tab.Screen name="Friends" component={Friends} />
+      <Tab.Screen name="Notifications" component={NotificationsList} />
+    </Tab.Navigator>
   );
 };
 
@@ -43,6 +53,7 @@ const BeforeLoginNav = () => {
       />
       <Stack.Screen name="LaunchPage" component={LaunchPage} />
       <Stack.Screen name="Play" component={QuizContainer} />
+      <Stack.Screen name="My Account" component={MyAccount} />
     </Stack.Navigator>
   );
 };
@@ -55,9 +66,11 @@ export default function App() {
     const logged = await AsyncStorage.getItem("isLoggedIn");
     const user = await AsyncStorage.getItem("userLogged");
 
+    console.log("logged :>> ", logged);
     if (logged) {
       setIsLoggedIn(true);
       setUserLogged(user);
+      console.log("isLoggedIn :>> ", isLoggedIn);
       console.log("userLogged inside getDataFromStorage :>> ", user);
     }
   };
@@ -66,7 +79,6 @@ export default function App() {
     getDataFromStorage();
   }, []);
 
-  console.log("userLogged app:>> ", userLogged);
   return (
     <NavigationContainer>
       {isLoggedIn ? <LoginNav /> : <BeforeLoginNav />}
