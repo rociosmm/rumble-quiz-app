33d417c426908d10be5530467677133a933a6d2c
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-07-04 17:04:44 +0100

    Added new navigation bar - unfinished

--- navigation/AppNavigation.jsx
+++ navigation/AppNavigation.jsx
@@ -1,66 +1,168 @@
-/* import React from "react";
-import { Image, Text, TouchableOpacity } from "react-native";
+import React from 'react';
 import {
-  AnimatedTabBarNavigator,
-  DotSize,
-  TabElementDisplayOptions,
-} from "react-native-animated-nav-tab-bar";
-import Icon from "react-native-vector-icons/Feather";
-import styled from "styled-components/native";
-import QuizContainer from "../Components/QuizContainer";
-import MyAccount from "../Components/MyAccount";
-import NotificationsList from "../Components/NotificationsList";
-import Friends from "../Components/Friends";
+  Alert,
+  Animated,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { CurvedBottomBarExpo } from 'react-native-curved-bottom-bar';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import QuizContainer from '../Components/QuizContainer';
+import NotificationsList from '../Components/NotificationsList';
+import Friends from '../Components/Friends';
+import {useNavigation} from '@react-navigation/native'
 
-const Tabs = AnimatedTabBarNavigator();
-
-/* const Screen = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #333333;
-`; */
-
-/*const TabBarIcon = ({ name, size = 24, tintColor }) => {
-  return <Icon name={name} size={size} color={tintColor} />;
+const Screen1 = () => {
+  return <View style={styles.screen1} />;
 };
 
-export default () => (
-  <Tabs.Navigator
-    initialRouteName="New game"
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color }) => {
-        let iconName;
+const Screen2 = () => {
+  return <View style={styles.screen2} />;
+};
 
-        if (route.name === "New game") {
-          iconName = "home";
-        } else if (route.name === "My account") {
-          iconName = "user";
-        } else if (route.name === "Notifications") {
-          iconName = "inbox";
-        } else if (route.name === "Friends") {
-          iconName = "users";
-        }
+export default function AppNavigation() {
+  const navigation = useNavigation()
+  const _renderIcon = (routeName, selectedTab) => {
+    let icon = '';
 
-        return (
-          <TabBarIcon focused={focused} tintColor={color} name={iconName} />
-        );
-      },
-      tabBarActiveTintColor: "#ff0000",
-      tabBarInactiveTintColor: "#ff0000",
-      tabBarActiveBackgroundColor: "red",
-    })}
-    appearance={{
-      shadow: true,
-      floating: true,
-      whenActiveShow: TabElementDisplayOptions.ICON_ONLY,
-      dotSize: DotSize.SMALL,
-    }}
-  >
-    <Tabs.Screen name="New game" component={QuizContainer} />
-    <Tabs.Screen name="Friends" component={Friends} />
-    <Tabs.Screen name="My account" component={MyAccount} />
-    <Tabs.Screen name="Notifications" component={NotificationsList} />
-  </Tabs.Navigator>
-);
- */
+    switch (routeName) {
+      case 'Play':
+        icon = 'extension-puzzle';
+        break;
+      case 'My Account':
+        icon = 'person';
+        break;
+        case 'Leaderboard':
+          icon = 'trophy';
+          break;
+          case 'Friends':
+            icon = 'people-sharp';
+            break;
+            case 'Notifications':
+            icon = 'notifications-sharp';
+            break;
+    }
+
+    return (
+      <Ionicons
+        name={icon}
+        size={25}
+        color={routeName === selectedTab ? 'black' : 'gray'}
+      />
+    );
+  };
+  const renderTabBar = ({ routeName, selectedTab, navigate }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => navigate(routeName)}
+        style={styles.tabbarItem}
+      >
+        {_renderIcon(routeName, selectedTab)}
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+  
+      <CurvedBottomBarExpo.Navigator
+        type="DOWN"
+        style={styles.bottomBar}
+        shadowStyle={styles.shawdow}
+        height={55}
+        circleWidth={50}
+        bgColor="white"
+        initialRouteName="Play"
+        borderTopLeftRight
+        renderCircle={({ selectedTab, navigate }) => (
+          <Animated.View style={styles.btnCircleUp}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("Play")}
+            >
+              <Ionicons name={'extension-puzzle'} color="gray" size={25} />
+            </TouchableOpacity>
+          </Animated.View>
+        )}
+        tabBar={renderTabBar}
+      >
+        <CurvedBottomBarExpo.Screen
+          name="Play"
+          position="CIRCLE"
+          component={() => <QuizContainer />}
+        />
+        <CurvedBottomBarExpo.Screen
+          name="Friends"
+          component={() => <Friends />}
+          position="RIGHT"
+        />
+        <CurvedBottomBarExpo.Screen
+          name="Notifications"
+          component={() => <NotificationsList />}
+          position="RIGHT"
+        />
+      </CurvedBottomBarExpo.Navigator>
+    
+  );
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  shawdow: {
+    shadowColor: '#DDDDDD',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 5,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  bottomBar: {},
+  btnCircleUp: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#E8E8E8',
+    bottom: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 1,
+  },
+  imgCircle: {
+    width: 30,
+    height: 30,
+    tintColor: 'gray',
+  },
+  tabbarItem: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    width: 30,
+    height: 30,
+  },
+  screen1: {
+    flex: 1,
+    backgroundColor: '#BFEFFF',
+  },
+  screen2: {
+    flex: 1,
+    backgroundColor: '#FFEBCD',
+  },
+});
