548e4212dad0891df44070a506ffd01849f897d1
rociosmm <rociosmm@yahoo.com>
2024-07-04 16:37:27 +0100

    display info from the userLogged

--- Components/LeaderBoard.jsx
+++ Components/LeaderBoard.jsx
@@ -1,25 +1,63 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import Leaderboard from 'react-native-leaderboard';
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import Leaderboard from "react-native-leaderboard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function LeaderBoard() {
-    const playersPointData = [{player_username: 'Joe', total_points: 52}, {player_username: 'Jenny', total_points: 38}, {player_username: 'Nikki', total_points: 72}]
+  const [userLogged, setUserLogged] = useState("Nikki");
+
+  const getUserLogged = async () => {
+    const user = await AsyncStorage.getItem("userLogged");
+    if (user) setUserLogged(user);
+  };
+
+  useEffect(() => {
+    //getUserLogged();
+  }, []);
+
+
+  const playersPointData = [
+    { player_username: "Joe", total_points: 52 },
+    { player_username: "Jenny", total_points: 38 },
+    { player_username: "Nikki", total_points: 2 },
+  ];
+
+  const myData = playersPointData.find(
+    (ply) => ply.player_username === userLogged
+  );
+
+  const position = playersPointData.findIndex(
+    (ply) => ply.player_username === userLogged
+  );
 
   return (
     <View>
-      <Text style={styles.title}>LeaderBoard</Text>
-      
-      <Leaderboard 
-        data={playersPointData} 
-        sortBy='total_points' 
-        labelBy='player_username'/>
+      <View style={styles.header}>
+        <Text style={styles.h2}>LeaderBoard</Text>
+        <Text style={styles.subtitle}>
+          My points: {myData.total_points}, position: {position + 1}
+        </Text>
+      </View>
+
+      <Leaderboard
+        data={playersPointData}
+        sortBy="total_points"
+        labelBy="player_username"
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-title: {
+  h2: {
     fontSize: 30,
-    textAlign: "center"
-}
-})
+    textAlign: "center",
+  },
+  header: {
+    backgroundColor: "#CCAE2F",
+  },
+  subtitle: {
+    fontSize: 26,
+    textAlign: "center",
+  },
+});
