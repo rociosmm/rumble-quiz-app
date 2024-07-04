260d000854468efacd95327dda516cdf79a40ca8
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-07-04 14:59:15 +0100

    Finished Stats section

--- Components/Stats.jsx
+++ Components/Stats.jsx
@@ -1,5 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'; 
+import { DataTable } from 'react-native-paper'; 
 
 export default function Stats({username}) {
 
@@ -15,7 +17,40 @@ export default function Stats({username}) {
     <View>
       <Text>Your Stats</Text>
       <Text>Total Games Played: {userStats.games_played}</Text>
-      
+    <DataTable style={styles.container}> 
+      <DataTable.Row> 
+        <DataTable.Cell style={styles.stat_title}>Total Points</DataTable.Cell> 
+        <DataTable.Cell style={styles.stat_data}>{userStats.total_points}</DataTable.Cell> 
+      </DataTable.Row> 
+      <DataTable.Row> 
+        <DataTable.Cell style={styles.stat_title}>Games Played</DataTable.Cell> 
+        <DataTable.Cell style={styles.stat_data}>{userStats.games_played}</DataTable.Cell> 
+      </DataTable.Row> 
+      <DataTable.Row> 
+        <DataTable.Cell style={styles.stat_title}>Games Won</DataTable.Cell> 
+        <DataTable.Cell style={styles.stat_data}>{userStats.games_won}</DataTable.Cell> 
+      </DataTable.Row> 
+      <DataTable.Row> 
+        <DataTable.Cell style={styles.stat_title}>Favourite Category</DataTable.Cell> 
+        <DataTable.Cell style={styles.stat_data}>{userStats.top_topic}</DataTable.Cell> 
+      </DataTable.Row> 
+    </DataTable> 
     </View>
+  
   )
 }
+
+const styles = StyleSheet.create({ 
+  container: { 
+    padding: 15, 
+  }, 
+  stat_title: {
+    backgroundColor: '#DCDCDC', 
+    paddingLeft: 20,
+  }, 
+  stat_data: {
+    paddingLeft: 20,
+    borderColor: 'grey',
+    borderWidth: 1
+  }
+});
