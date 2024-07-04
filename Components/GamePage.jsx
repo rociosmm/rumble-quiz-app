8a75100dd297038586ead2b1abc29ed01322cb32
Alex <alexdtaylor93@gmail.com>
2024-07-04 15:34:14 +0100

    added a question card component to be rendered on the game page

---
+++ Components/GamePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function GamePage() {
+  return (
+  <View>
+
+  </View>
+  );
+}
