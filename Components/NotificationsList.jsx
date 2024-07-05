31e97bbc7b3012a50e063b441bb58e0115514258
Alex <alexdtaylor93@gmail.com>
2024-07-05 13:42:59 +0100

    added avatar component only hard coded so far

--- Components/NotificationsList.jsx
+++ Components/NotificationsList.jsx
@@ -3,11 +3,30 @@ import React from "react";
 import { useCountdown } from "react-native-countdown-circle-timer";
 import UrgeWithPleasureComponent from "./Countdown";
 import QuestionCard from "./QuestionCard";
+import PlayerAvatars from "./PlayerAvatars";
 
 export default function NotificationsList() {
+  const players = [
+    {
+      username: "George",
+      avatarUrl:
+        "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-bear-48.png?raw=true",
+    },
+    {
+      username: "Eve",
+      avatarUrl:
+        "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-gorilla-48.png?raw=true",
+    },
+    {
+      username: "Daniel",
+      avatarUrl:
+        "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-lion-48.png?raw=true",
+    },
+  ];
   return (
     <View>
       <Text>NotificationsList</Text>
+      <PlayerAvatars />
       <UrgeWithPleasureComponent />
       <QuestionCard />
     </View>
