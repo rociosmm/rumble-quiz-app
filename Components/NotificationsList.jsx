fed4fce2b1f0a11970ed793d6787ffd953f4d380
Alex <alexdtaylor93@gmail.com>
2024-07-05 14:48:51 +0100

    rendered a progress bar to track the round

--- Components/NotificationsList.jsx
+++ Components/NotificationsList.jsx
@@ -1,9 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useCountdown } from "react-native-countdown-circle-timer";
 import UrgeWithPleasureComponent from "./Countdown";
 import QuestionCard from "./QuestionCard";
 import PlayerAvatars from "./PlayerAvatars";
+import ProgressBar from "./ProgressBar";
 
 export default function NotificationsList() {
   const players = [
@@ -29,6 +29,7 @@ export default function NotificationsList() {
       <PlayerAvatars />
       <UrgeWithPleasureComponent />
       <QuestionCard />
+      <ProgressBar />
     </View>
   );
 }
---
+++ Components/ProgressBar.jsx
@@ -0,0 +1,25 @@
+import * as Progress from "react-native-progress";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+export default function ProgressBar() {
+  return (
+    <View style={styles.progressBarContainer}>
+      <Progress.Bar
+        progress={1}
+        width={300}
+        height={20}
+        borderRadius={10}
+        color={"#1e90ff"}
+        unfilledColor={"#d3d3d3"}
+        borderWidth={0}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  progressBarContainer: {
+    marginVertical: 5,
+    alignSelf: "center",
+  },
+});
