accfdd310e15a7c00c39b131295411ea15da1bbb
Alex <alexdtaylor93@gmail.com>
2024-07-09 12:18:28 +0100

    general styling changes made

--- Components/NotificationsList.jsx
+++ Components/NotificationsList.jsx
@@ -6,30 +6,9 @@ import PlayerAvatars from "./PlayerAvatars";
 import ProgressBar from "./ProgressBar";
 
 export default function NotificationsList() {
-  const players = [
-    {
-      username: "George",
-      avatarUrl:
-        "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-bear-48.png?raw=true",
-    },
-    {
-      username: "Eve",
-      avatarUrl:
-        "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-gorilla-48.png?raw=true",
-    },
-    {
-      username: "Daniel",
-      avatarUrl:
-        "https://github.com/nkytruong/rumble-quiz-app/blob/main/assets/avatars/icons8-lion-48.png?raw=true",
-    },
-  ];
   return (
     <View>
       <Text>NotificationsList</Text>
-      <PlayerAvatars />
-      <UrgeWithPleasureComponent />
-      <QuestionCard />
-      <ProgressBar />
     </View>
   );
 }
