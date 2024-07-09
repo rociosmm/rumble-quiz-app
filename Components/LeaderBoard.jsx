ec488d76a29b43cf713910ec6c8d4311c980ebd9
Nikki Truong <nikki@Nikkis-MacBook-Pro.local>
2024-07-09 10:59:55 +0100

    Added axios request to retrieve leaderboard data

--- Components/LeaderBoard.jsx
+++ Components/LeaderBoard.jsx
@@ -2,44 +2,71 @@ import { View, Text, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 import Leaderboard from "react-native-leaderboard";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getUsersPoints } from "../utils/api";
 
 export default function LeaderBoard() {
-  const [userLogged, setUserLogged] = useState("Nikki");
+  const [userLogged, setUserLogged] = useState("");
+  const [usersPoints, setUsersPoints] = useState([])
 
   const getUserLogged = async () => {
-    const user = await AsyncStorage.getItem("userLogged");
-    if (user) setUserLogged(user);
+    try {
+      const user = await AsyncStorage.getItem("userLogged");
+      if (user) {
+        setUserLogged(user)
+        console.log(userLogged, "<<<userLeaderboard")
+      };
+    } catch (error) {
+      console.error("Error retrieving user from AsyncStorage", error);
+    }
   };
 
   useEffect(() => {
-    // getUserLogged();
+    getUserLogged();
   }, []);
 
-  const playersPointData = [
-    { player_username: "Joe", total_points: 52 },
-    { player_username: "Jenny", total_points: 38 },
-    { player_username: "Nikki", total_points: 2 },
-  ];
+  useEffect(() => {
+    if(userLogged){
+      getUsersPoints().then(({leaderboard}) => {
+        setUsersPoints(leaderboard)
+      })
+    }
+  }, [userLogged])
+  const currentPlayerScore = usersPoints.find((user) => {
+    return user.player_username === userLogged
+  })
+  console.log(currentPlayerScore, '<<<currentPlayerScore')
+  
+  // const playersPointData = [
+  //   { player_username: "Joe", total_points: 52 },
+  //   { player_username: "Jenny", total_points: 38 },
+  //   { player_username: "Nikki", total_points: 2 },
+  // ];
 
-  const myData = playersPointData.find(
-    (ply) => ply.player_username === userLogged
-  );
+  // const myData = playersPointData.find(
+  //   (ply) => ply.player_username === userLogged
+  // );
 
-  const position = playersPointData.findIndex(
-    (ply) => ply.player_username === userLogged
-  );
+  // const position = playersPointData.findIndex(
+  //   (ply) => ply.player_username === userLogged
+  // );
+
+  // const currentPlayerScore = usersPoints.filter((userPoints) => userPoints.player_username === userLogged)
+  // console.log(currentPlayerScore)
 
   return (
     <View>
       <View style={styles.header}>
         <Text style={styles.h2}>LeaderBoard</Text>
         <Text style={styles.subtitle}>
-          My points: {myData.total_points}, position: {position + 1}
+          {userLogged? currentPlayerScore.total_points : null}
+          {/* My points: {myData.total_points}, position: {position + 1} */}
         </Text>
+        <Text style={styles.pointsText}>Points</Text>
+      
       </View>
 
       <Leaderboard
-        data={playersPointData}
+        data={usersPoints}
         sortBy="total_points"
         labelBy="player_username"
       />
@@ -56,7 +83,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#CCAE2F",
   },
   subtitle: {
-    fontSize: 26,
+    fontSize: 50,
     textAlign: "center",
   },
+  pointsText: {
+    fontSize: 25,
+    textAlign: 'center'
+  }
 });
