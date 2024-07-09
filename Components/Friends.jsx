7ca290cae104bb795239c0813c95b890c1862571
rociosmm <rociosmm@yahoo.com>
2024-07-09 11:26:42 +0100

    friends fetched

--- Components/Friends.jsx
+++ Components/Friends.jsx
@@ -1,13 +1,66 @@
 import { View, Text } from "react-native";
-import React from "react";
-import LeaderBoard from "./LeaderBoard";
-import Endpoints from "./endpoints";
+import React, { useState, useEffect } from "react";
+import { getFriends, getUserByUsername } from "../utils/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import CustomStyles from "../Styles/CustomStyles";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Friends() {
+  const [userLogged, setUserLogged] = useState("");
+  const [friends, setFriends] = useState([]);
+  const [friendsDetails, setFriendsDetails] = useState([]);
+
+  const getUserLogged = async () => {
+    try {
+      const user = await AsyncStorage.getItem("userLogged");
+      setUserLogged(user);
+    } catch (error) {
+      console.error("Error retrieving user from AsyncStorage", error);
+    }
+  };
+
+  useEffect(() => {
+    getUserLogged();
+  }, []);
+
+  useEffect(() => {
+    if (userLogged) {
+      getFriends(userLogged).then((data) => {
+        console.log("data friends endpoint :>> ", data);
+        const friends_usernames = data.map((fr) => fr.user2_username);
+        setFriends(friends_usernames);
+      });
+    }
+  }, [userLogged]);
+
+  useEffect(() => {
+    if (friends.length > 0) {
+      const fetchFriendsDetails = async () => {
+        const details = await Promise.all(
+          friends.map((friend) => getUserByUsername(friend))
+        );
+        const users = details.map(({ user }) => user);
+        setFriendsDetails(users);
+      };
+      fetchFriendsDetails();
+    }
+  }, [friends]);
+
   return (
-    <View>
-      <Text>Friends</Text>
-      <LeaderBoard />
-    </View>
+    <SafeAreaView>
+      <View style={CustomStyles.topBanner}>
+        <Text style={CustomStyles.h2}>Friends</Text>
+      </View>
+      <View style={{ paddingVertical: 20 }}>
+        {friendsDetails.map((friend) => {
+          console.log("friend indv :>> ", friend);
+          return (
+            <Text style={CustomStyles.h3} key={friend.username}>
+              {friend.username}
+            </Text>
+          );
+        })}
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ Styles/CustomStyles.js
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default CustomStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  topBanner: {
+    width: "100%",
+    paddingVertical: 70,
+    backgroundColor: "#ccae2f"
+  },
+  h1: {
+    fontSize: 34,
+    fontWeight: "bold",
+  },
+  h2: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  h3: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
